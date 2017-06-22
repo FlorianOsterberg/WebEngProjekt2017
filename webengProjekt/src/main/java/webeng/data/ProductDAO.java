c4bd728b0ec4e6902dd1547a0da51f1f437f5651
Florian Osterberg <florian.osterberg@web.de>
2017-06-22 13:46:57 +0200

    zwischen push

---
+++ webengProjekt/src/main/java/webeng/data/ProductDAO.java
@@ -0,0 +1,10 @@
+package webeng.data;
+
+
+import java.util.List;
+
+public interface ProductDAO {
+
+
+	
+}//end interface ProductDAO
---
+++ webengProjekt/src/main/java/webeng/data/TransactionDAO.java
@@ -0,0 +1,5 @@
+package webeng.data;
+
+public class TransactionDAO {
+
+}
