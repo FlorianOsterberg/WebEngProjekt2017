9907b0ee7a801c2b3fddd7cd706e4b6cb8ad2cce
Max <pallaschmaximilian@gmail.com>
2017-06-22 13:28:45 +0200

    Finished it!

--- webengProjekt/src/main/java/webeng/transferobjects/Product.java
+++ webengProjekt/src/main/java/webeng/transferobjects/Product.java
@@ -2,6 +2,43 @@ package webeng.transferobjects;
 
 public class Product {
 
+	private int id;
+	private String name;
+	private float price;
+	private String category;
+	private String description;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public float getPrice() {
+		return price;
+	}
+	public void setPrice(float price) {
+		this.price = price;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
 	
 	
 }
