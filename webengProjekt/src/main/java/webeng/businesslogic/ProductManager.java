4c453c00f7a0853d308a45a09f810335a72230d0
Max <pallaschmaximilian@gmail.com>
2017-06-22 16:29:05 +0200

    Added methods to Product

--- webengProjekt/src/main/java/webeng/businesslogic/ProductManager.java
+++ webengProjekt/src/main/java/webeng/businesslogic/ProductManager.java
@@ -1,15 +1,28 @@
 package webeng.businesslogic;
 
+import java.util.List;
+
 import webeng.data.DAOFactory;
 import webeng.data.ProductDAO;
 import webeng.data.DAOFactory.Backend;
+import webeng.transferobjects.Product;
+import webeng.transferobjects.User;
 
 public class ProductManager {
 
-	ProductDAO productDao;
+	ProductDAO productDAO;
 	
 	public ProductManager() {
-		productDao = DAOFactory.getDAOFactory(Backend.H2).getProductDAO();
+		productDAO = DAOFactory.getDAOFactory(Backend.H2).getProductDAO();
+	}
+
+	public void addProduct(int id, String name, float price, String category, String description){
+		
+		Product newProduct = new Product(id,name,price,category,description);
+		productDAO.addProduct(newProduct);
 	}
 	
+	public List<Product> getProducts() {
+		return productDAO.getProducts();
+	}
 }
