f9ace4417c3338d7b727e0b9aa15c16bbe2f56cf
Florian Osterberg <florian.osterberg@web.de>
2017-06-22 15:30:17 +0200

    transaction manager

--- webengProjekt/src/main/java/webeng/businesslogic/TransactionManager.java
+++ webengProjekt/src/main/java/webeng/businesslogic/TransactionManager.java
@@ -1,7 +1,10 @@
 package webeng.businesslogic;
 
+import java.util.List;
+
 import webeng.data.DAOFactory;
 import webeng.data.DAOFactory.Backend;
+import webeng.transferobjects.Transaction;
 import webeng.data.TransactionDAO;
 
 public class TransactionManager {
@@ -10,6 +13,21 @@ public class TransactionManager {
 	
 	public TransactionManager() {
 		transactionDao = DAOFactory.getDAOFactory(Backend.H2).getTransactionDAO();
+	}
+	
+	public void addTransaction(Transaction transaction) {
+		
+		transactionDao.addTransaction(transaction);
+		
+	}
+	
+	public Transaction getTransaction(Transaction transaction) {
+		return transactionDao.getTransaction(transaction);
+	}
+	
+	public List<Transaction> findTransactions() {
+		
+		return transactionDao.findTransactions();
 	}
 	
 }
