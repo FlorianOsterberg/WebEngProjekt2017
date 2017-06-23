d020efa3ffe65ebd0fa47a4883a843294c125cb8
Florian Osterberg <florian.osterberg@web.de>
2017-06-23 12:28:31 +0200

    user bean

--- webengProjekt/src/main/java/webeng/presentation/managedbeans/UserBean.java
+++ webengProjekt/src/main/java/webeng/presentation/managedbeans/UserBean.java
@@ -4,22 +4,38 @@ package webeng.presentation.managedbeans;
 import java.io.Serializable;
 import java.util.Date;
 
+import javax.annotation.PostConstruct;
 import javax.faces.bean.ApplicationScoped;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.RequestScoped;
 
+import webeng.businesslogic.UserManager;
+import webeng.transferobjects.User;
+
 @ManagedBean(name = "userBean")
 @RequestScoped
 public class UserBean implements Serializable {
 	
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
+	UserManager manager;
+	User user;
 
 	public UserBean() {
-	       
-	   }
+		
+	}
+	
+	public User getUser() {
+		return user;
+	}
+	
+	public void setUser(User user) {
+		this.user = user;
+	}
+	
+	@PostConstruct
+	public void init() {
+		
+	}
 	
 }//end class UserBean
